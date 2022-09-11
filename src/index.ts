54efa30a1b9bb936c5d91712f7da16f2cd31ed5d
Joe Ma <rikkaneko23@gmail.com>
2022-09-12 00:02:27 +0800

    Initial commit
    Add generation of QR Code of UTF8 and SVG

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
+/*
+ * This file is part of qrcode-gen.
+ * Copyright (c) 2022 Joe Ma <rikkaneko23@gmail.com>
  *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Learn more at https://developers.cloudflare.com/workers/
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import QRSvg from 'qrcode';
